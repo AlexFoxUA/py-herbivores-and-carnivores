4f4939f05e923a34ae8e8db4da9c10378dc14a70
Oleksandr Vilenskiy <128734566+AlexFoxUA@users.noreply.github.com>
2025-11-26 18:29:03 +0200

    Sol Animal 1

--- app/main.py
+++ app/main.py
@@ -1,1 +1,36 @@
-# write your code here
+class Animal:
+
+    alive = []
+
+    def __init__(
+        self,
+        name: str = 'Fox',
+        health: int = 100,
+        hidden: bool = False
+    ) -> None:
+        self.name = name
+        self.hidden = hidden
+        self.health = health
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore_obj) -> None:
+        if isinstance(herbivore_obj, Herbivore) and not herbivore_obj.hidden:
+            herbivore_obj.health -= 50
+
+        if herbivore_obj in Animal.alive and herbivore_obj.health <= 0:
+            Animal.alive.remove(herbivore_obj)
